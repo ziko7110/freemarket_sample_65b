faf1f357d4158860d252dfc5e06ca72d10a2f768
shenye76 <shinyainoue76@gmail.com>
2020-01-08 00:05:20 +0900

    optional:trueの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   validates :name, :price, presence: true
-  belongs_to :user, optional: true
-  belongs_to :seller, class_name: "User", optional: true
-  belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :user
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
   has_many :photos
   # accepts_nested_attributes_for :images, allow_destroy: true
 end

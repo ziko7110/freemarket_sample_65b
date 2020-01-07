1d45034afdf19c1fc6ecfa1e926abfe5532621e1
ziko7110 <ziko7110@gmail.com>
2020-01-07 14:27:11 +0900

    テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
   has_many :user_address
   accepts_nested_attributes_for :user_address
+  validates :nickname,:encrypted_password,:email,:family_name_kanji,:first_name_kanji,:family_name_kana,:first_name_kana,:birth_year,:birth_month,:birth_day,:mobile_phone_number,   presence: true
+  validates :email, uniqueness: true
+  validates :family_name_kana,:first_name_kana, presence: true, format: { with: /\p{Hiragana}/, \
+  message: 'には日本語を含めてください。(Text must contain hiragana.)' }
+  validates :mobile_phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}-[0-9]{4}\z/}
+  validates :email, format: { with: /\A[a-zA-Z]+\z/}
 end
+

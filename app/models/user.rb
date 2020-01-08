4faf715475e939da299b950dd2a80e2bd6f135c7
obSYO <sho020202forest@gmail.com>
2020-01-08 13:10:43 +0900

    sns_credentialにアソシエーション記述、ルーティング にomniauthのコントローラー記述、リンクをhamlに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,51 @@ class User < ApplicationRecord
   validates :family_name_kana,:first_name_kana, presence: true, format: { with: /\p{katakana}/, \
   message: 'には日本語を含めてください。(Text must contain katakana.)' }
 
+  def self.without_sns_data(auth)
+    user = User.where(email: auth.info.email).first
+
+      if user.present?
+        sns = SnsCredential.create(
+          uid: auth.uid,
+          provider: auth.provider,
+          user_id: user.id
+        )
+      else
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email,
+        )
+        sns = SnsCredential.new(
+          uid: auth.uid,
+          provider: auth.provider
+        )
+      end
+      return { user: user ,sns: sns}
+    end
+
+   def self.with_sns_data(auth, snscredential)
+    user = User.where(id: snscredential.user_id).first
+    unless user.present?
+      user = User.new(
+        nickname: auth.info.name,
+        email: auth.info.email,
+      )
+    end
+    return {user: user}
+   end
+
+   def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = with_sns_data(auth, snscredential)[:user]
+      sns = snscredential
+    else
+      user = without_sns_data(auth)[:user]
+      sns = without_sns_data(auth)[:sns]
+    end
+    return { user: user ,sns: sns}
+  end
 end
 

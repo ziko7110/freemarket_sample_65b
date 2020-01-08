d35cd0991f1cbd6eb0dc0dad36967ce2ae13af6e
nisidahiroaki <57944481+nisidahiroaki@users.noreply.github.com>
2020-01-08 20:09:01 +0900

    商品詳細　ユーザー名

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User"
   has_many :photos
+  belongs_to :user
   # accepts_nested_attributes_for :photos
   # accepts_nested_attributes_for :photos, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
-         has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
-         has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
+  has_many :items      
+
+        has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+        has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "seller_id", class_name: "Item"
+        has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "seller_id", class_name: "Item"
 end

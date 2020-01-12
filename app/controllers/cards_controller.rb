f436aa9546b8aac04abe1365ea400bbeedb1e1e7
obSYO <sho020202forest@gmail.com>
2020-01-12 17:57:13 +0900

    環境変数をcredentialに追加、ENVファイルでの呼び出しからcredentialでの呼び出しに記述変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -11,7 +11,7 @@ class CardsController < ApplicationController
   end
     # ここはDB実装の際に使います
   def pay #payjpとCardのデータベース作成を実施します。
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
@@ -34,7 +34,7 @@ class CardsController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     if card.blank?
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(card.customer_id)
       customer.delete
       card.delete
@@ -47,7 +47,7 @@ class CardsController < ApplicationController
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -9,7 +9,7 @@ class PurchaseController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: “cards”, action: “new”
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(@card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -18,7 +18,7 @@ class PurchaseController < ApplicationController
   end
   
   def pay
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    Payjp.api_key = Rails.application.credentials[:payjp][:PAYJP_PRIVATE_KEY]
     Payjp::Charge.create(
     amount: @item.price,          #支払金額を引っ張ってくる
     customer: @card.customer_id,  #顧客ID

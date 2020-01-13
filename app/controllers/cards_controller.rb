c41ad0c78ccc484d133766032e6d348af26dd6c5
obSYO <sho020202forest@gmail.com>
2020-01-13 10:33:46 +0900

    クレジットカード保存時のページ遷移先変更

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -23,7 +23,7 @@ class CardsController < ApplicationController
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to user_password_path
+        redirect_to root_path
       else
         redirect_to action: "pay"
       end
